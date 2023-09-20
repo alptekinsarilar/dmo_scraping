13834cca1f853249e2052d7fc4639d5f8ee172c4
Alptekin <alptekinsarilar@gmail.com>
2023-09-20 11:45:40 +0300

    whole electronic category scraping completed

--- scrape.py
+++ scrape.py
@@ -5,72 +5,82 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font
 
-try:
-    #source = requests.get("https://www.dmo.gov.tr/Katalog/Urun/Detay/1171329_1059351")
-    #source.raise_for_status()
-    #soup = BeautifulSoup(source.text, "html.parser")
-    
-    with open("ornek_urun.html", "r") as file:
-        soup = BeautifulSoup(file, "html.parser")
-    
-    """ Part 1 """
-    
-    baslik = soup.find("div", class_="title").text
-    tedarikci = soup.find("a", class_="available").text.strip()
-    aciklama = soup.find_all("p")[0].string
-    vergisiz_fiyat = soup.find("div", class_="price-current").text
-    vergili_fiyat = soup.find("div", class_="price-prev").text
-    
-    birim_fiyat = soup.find("div", class_="excerpt").find_all("div")[8].text.strip()
-    kdv = soup.find("div", class_="excerpt").find_all("div")[9].text.strip()
-    dmo_urun_kodu = soup.find("div", class_="excerpt").find_all("div")[10].text.strip()
-    olcu_birimi = soup.find("div", class_="excerpt").find_all("div")[11].text.strip()
-    orijinal_urun_kodu = soup.find("div", class_="excerpt").find_all("div")[12].text.strip()
-    garanti_suresi = soup.find("div", class_="excerpt").find_all("div")[13].text.strip()
-    yerli_katki_orani = soup.find("div", class_="excerpt").find_all("div")[14].text.strip()
-    teknolojik_duzey = soup.find("div", class_="excerpt").find_all("div")[15].text.strip()
-    yerli_mali_belgesi_gecerlilik_tarihi = soup.find("div", class_="excerpt").find_all("div")[16].text.strip()
-    
-    #print(baslik)
-    #print(tedarikci)
-    #print()
-    #print(aciklama)
-    #print(vergisiz_fiyat)
-    #print(vergili_fiyat)
-    
-    #print(birim_fiyat)
-    #print(kdv)
-    #print(dmo_urun_kodu)
-    #print(olcu_birimi)
-    #print(orijinal_urun_kodu)
-    #print(garanti_suresi)
-    #print(yerli_katki_orani)
-    #print(teknolojik_duzey)
-    #print(yerli_mali_belgesi_gecerlilik_tarihi)
-    
-    """ Part 2 """
-    
-    satis_kosullari = soup.find("section", class_="fadeInUp").text.strip()
-    
-    #print(satis_kosullari)
-    
-    """ Part 3 """
-    
-    urun_ozellikleri = soup.find_all("section", class_="fadeInUp")[1].text.strip()
-    
-    #print(urun_ozellikleri)
-    
-    
-    """ Excel Part """
-    #wb = Workbook()
-    #ws = wb.active
-    #ws.title = "Elektronik"
-    #
-    #headings = ["Ürün Bilgisi", "Satış Koşulları", "Ürün Özellikleri"]
-    #ws.append(headings)
-    #ws.append(["üb", "sk", "üö"])
-    #
-    #wb.save("test.xlsx")
-    
-except Exception as e:
-    print(e)
+wb = Workbook()
+ws = wb.active
+
+headings = ["Ürün Bilgisi", "Satış Koşulları", "Ürün Özellikleri"]
+ws.append(headings)
+wb.save("elektronik.xlsx")
+
+def scrape_product(url):
+    try:
+        source = requests.get("https://www.dmo.gov.tr" + url)
+        source.raise_for_status()
+        soup = BeautifulSoup(source.text, "html.parser")
+
+        #with open("ornek_urun.html", "r") as file:
+        #    soup = BeautifulSoup(file, "html.parser")
+
+        """ Part 1 """
+
+        baslik = soup.find("div", class_="title").text
+        tedarikci = soup.find("a", class_="available").text.strip()
+        aciklama = soup.find_all("p")[0].string
+        vergisiz_fiyat = soup.find("div", class_="price-current").text
+        vergili_fiyat = soup.find("div", class_="price-prev").text
+
+        birim_fiyat = soup.find("div", class_="excerpt").find_all("div")[8].text.strip()
+        kdv = soup.find("div", class_="excerpt").find_all("div")[9].text.strip()
+        dmo_urun_kodu = soup.find("div", class_="excerpt").find_all("div")[10].text.strip()
+        olcu_birimi = soup.find("div", class_="excerpt").find_all("div")[11].text.strip()
+        orijinal_urun_kodu = soup.find("div", class_="excerpt").find_all("div")[12].text.strip()
+        garanti_suresi = soup.find("div", class_="excerpt").find_all("div")[13].text.strip()
+        yerli_katki_orani = soup.find("div", class_="excerpt").find_all("div")[14].text.strip()
+        teknolojik_duzey = soup.find("div", class_="excerpt").find_all("div")[15].text.strip()
+        yerli_mali_belgesi_gecerlilik_tarihi = soup.find("div", class_="excerpt").find_all("div")[16].text.strip()
+
+        strings = [baslik, tedarikci, aciklama, vergisiz_fiyat, vergili_fiyat, birim_fiyat, kdv, dmo_urun_kodu, olcu_birimi, orijinal_urun_kodu, garanti_suresi, yerli_katki_orani, teknolojik_duzey, yerli_mali_belgesi_gecerlilik_tarihi]
+
+        genel_bilgi = ""
+        for s in strings:
+            if s is not None:
+                genel_bilgi += s + "\n"
+        
+        #print(baslik)
+        #print(tedarikci)
+        #print()
+        #print(aciklama)
+        #print(vergisiz_fiyat)
+        #print(vergili_fiyat)
+
+        #print(birim_fiyat)
+        #print(kdv)
+        #print(dmo_urun_kodu)
+        #print(olcu_birimi)
+        #print(orijinal_urun_kodu)
+        #print(garanti_suresi)
+        #print(yerli_katki_orani)
+        #print(teknolojik_duzey)
+        #print(yerli_mali_belgesi_gecerlilik_tarihi)
+
+        """ Part 2 """
+
+        satis_kosullari = soup.find("section", class_="fadeInUp").text.strip()
+
+        #print(satis_kosullari)
+
+        """ Part 3 """
+
+        urun_ozellikleri = soup.find_all("section", class_="fadeInUp")[1].text.strip()
+
+        #print(urun_ozellikleri)
+
+
+        """ Excel Part """
+        ws.append([genel_bilgi, satis_kosullari, urun_ozellikleri])
+        
+        wb.save("test.xlsx")
+
+    except Exception as e:
+        print(e)
+        
--- scrape_katalog.py
+++ scrape_katalog.py
@@ -1,29 +1,58 @@
 from bs4 import BeautifulSoup
 import requests
+import math
+from scrape import scrape_product
 
-with open("ornek_katalog.html", "r") as file:
-    soup = BeautifulSoup(file, "html.parser")
+def scrape_catalog_with_page_number(page_no):
+    try:
+        source = requests.get(f"https://www.dmo.gov.tr/Arama?s=&k=%7c%7cElektronik&p={page_no}&d=SM&e=3")
+        source.raise_for_status()
+        soup = BeautifulSoup(source.text, "html.parser")
 
-# Find all the <div> elements with class "product-item"
-product_items = set(soup.find_all('div', class_='product-item'))
+        # Find all the <div> elements with class "product-item"
+        product_items = soup.find_all('div', class_='product-item')
 
-# Initialize a list to store the extracted href values
-href_values = []
+        # Initialize a list to store the extracted href values
+        href_values = []
 
-# Loop through each product item and extract the href value
-for product_item in product_items:
-    # Find the <a> element within the product item
-    a_element = product_item.find('a', href=True)  # Check for the presence of href attribute
-    if a_element:
-        href_value = a_element['href']  # Extract the href attribute value
-        href_values.append(href_value)  # Append to the list
+        # Loop through each product item and extract the href value
+        for product_item in product_items:
+            # Find the <a> element within the product item
+            a_element = product_item.find('a', href=True)  # Check for the presence of href attribute
+            if a_element:
+                href_value = a_element['href']  # Extract the href attribute value
+                href_values.append(href_value)  # Append to the list
+                
+        # Somehow for 32 product there were 64 href so we need to make them unique
+        # Probably there are 2 href for a product, 1 for image 1 for text
+        href_values = set(href_values)
 
-href_values = set(href_values)
+        # Print the extracted href values
+        for href_value in href_values:
+            scrape_product(href_value)
+            
+    except Exception as e:
+            print(e)
+            
+def get_total_product_number():
+    try:
+        source = requests.get("https://www.dmo.gov.tr/Arama?k=%7c%7cElektronik")
+        source.raise_for_status()
+        soup = BeautifulSoup(source.text, "html.parser")
 
-# Print the extracted href values
-for href_value in href_values:
-    print(href_value)
-    
-# Call scrape.py for every href_value
+        # Find all the <div> elements with class "product-item"
+        result_counter = soup.find('div', class_='result-counter')
+        
+        number = result_counter.find('span').text
+        
+        return int(number)
+    except Exception as e:
+            print(e)
 
-# Create elektronik.py, ofis_kirtasiye.py etc. 
+total_page_no = math.ceil(get_total_product_number() / 32)
+
+# Now we need to scrape every page from "1" to "math.ceil(get_total_product_number() / 32)"
+for page_no in range(1, total_page_no+1):
+    print("page no in catalog:", page_no)
+    scrape_catalog_with_page_number(page_no)
+
